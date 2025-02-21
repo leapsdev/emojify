d92c99ab1278ce0a4b9ca2e90196f76ea7a13713
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-02-22 01:52:10 +0900

    delete:不要コンポーネント削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Input } from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
+
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <Button size="lg">Shadcn Button</Button>
-      <Checkbox />
-      <Input />
-      <Textarea />
+      <Button size="lg">Button</Button>
     </div>
   );
 }

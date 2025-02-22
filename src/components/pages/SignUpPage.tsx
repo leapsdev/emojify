ae4cc003d6609ab5372a7b1cd9b999a72f022238
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-02-23 03:40:24 +0900

    🔧 fix: インストールボタンとサインアップページのクライアントサイドディレクティブを調整
    - installButton.tsxに'use client'ディレクティブを追加
    - SignUpPage.tsxから'use client'ディレクティブを削除
    - コンポーネントの適切なレンダリングモードを確保

--- src/components/pages/SignUpPage.tsx
+++ src/components/pages/SignUpPage.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { FC } from "react"
 import { DynamicFloatingEmojis } from "@/components/signup/FloatingEmojis"
 import Header from "@/components/signup/Header"
@@ -9,10 +7,8 @@ import Footer from "@/components/signup/Footer"
 const SignUpPage: FC = () => {
   return (
     <main className="min-h-screen bg-white relative overflow-hidden">
-      {/* 浮遊する要素 */}
       <DynamicFloatingEmojis />
 
-      {/* メインコンテンツ */}
       <div className="relative z-10 flex flex-col min-h-screen p-6">
         <Header />
         <MainContent />

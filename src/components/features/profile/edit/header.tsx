d394fd6b75958881a87f6d735d696cdcd312c933
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-03-18 03:01:32 +0900

    🎨 style: プロフィール編集の戻るボタンを左寄せに修正
    - 戻るボタンの位置を左端に固定
    - レイアウトの整合性を改善

--- src/components/features/profile/edit/header.tsx
+++ src/components/features/profile/edit/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export const ProfileEditHeader = () => {
   return (
     <div className="relative flex items-center justify-center p-4 border-b">
-      <Link href="/profile" className="text-2xl">
+      <Link href="/profile" className="text-2xl absolute left-4">
         👈
       </Link>
     </div>

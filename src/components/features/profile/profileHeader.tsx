4b84a71c46751af4c64ae64fac1bb37097055ebd
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-03-30 08:16:52 +0900

    🔥 remove: ProfileHeaderコンポーネントを削除
    - プロフィールページのヘッダーを共通のヘッダーコンポーネントに統合したため、ProfileHeaderを削除しました。

--- src/components/features/profile/profileHeader.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { Header } from '@/components/shared/layout/header';
-
-export function ProfileHeader() {
-  return <Header centerContent={<span className="text-xl">👤</span>} />;
-}

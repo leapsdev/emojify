3af52d6f91a477ad223362097fdacbb6d2eee68f
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-04-03 02:26:14 +0900

    ✨ feat: EmojiListコンポーネントでインデックスプロパティを削除

--- src/components/features/explore/components/emojiList.tsx
+++ src/components/features/explore/components/emojiList.tsx
@@ -5,8 +5,8 @@ export function EmojiList() {
   return (
     <div className="p-2 flex-1">
       <div className="grid grid-cols-2 gap-2">
-        {EMOJI_ITEMS.map((item, index) => (
-          <EmojiItem key={item.id} item={item} index={index} />
+        {EMOJI_ITEMS.map((item) => (
+          <EmojiItem key={item.id} item={item} />
         ))}
       </div>
     </div>

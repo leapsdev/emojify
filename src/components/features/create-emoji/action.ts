56afc9eabba8849339c3f36131ce4a6f81502570
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-04-13 01:08:14 +0900

    🚧 fix: IPFSアップロード時のファイル処理を修正
    - FileオブジェクトをArrayBufferに変換してアップロード
    - Bufferを使用して適切なデータ形式に変換

--- src/components/features/create-emoji/action.ts
+++ src/components/features/create-emoji/action.ts
@@ -16,8 +16,12 @@ export async function uploadToIPFS(formData: FormData): Promise<UploadResult> {
       throw new Error('ファイルが見つかりません');
     }
 
+    // ファイルをArrayBufferに変換
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
     const sdk = getSDK();
-    const uri = await sdk.storage.upload(file, {
+    const uri = await sdk.storage.upload(buffer, {
       uploadWithGatewayUrl: true
     });
 

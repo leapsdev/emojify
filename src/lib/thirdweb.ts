9d222d9064ce15a78d346713fbcd27e9a553fa72
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-04-13 01:26:10 +0900

    Revert "🔧 fix: Sepolia Testnetの設定を改善"
    This reverts commit 2eb0e139fbb538123f2fe0c9531838826cdfa956.

--- src/lib/thirdweb.ts
+++ src/lib/thirdweb.ts
@@ -28,14 +28,7 @@ export const getSDK = () => {
   if (!process.env.THIRDWEB_SECRET_KEY) {
     throw new Error('THIRDWEB_SECRET_KEY is not defined');
   }
-  
-  // Sepolia testnetの設定
-  const sepolia = {
-    ...Sepolia,
-    rpc: ["https://rpc.sepolia.org"], // バックアップRPCを追加
-  };
-
-  return new ThirdwebSDK(sepolia, {
+  return new ThirdwebSDK(Sepolia, {
     secretKey: process.env.THIRDWEB_SECRET_KEY,
   });
 };

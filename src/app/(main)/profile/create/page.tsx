ecfc012f607b7965bb3fd157c0a853a8e3a9f7d6
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-04-20 22:51:42 +0900

    ✨ 動的レンダリングを強制するために、create/profileおよびrefreshページに'dynamic'エクスポートを追加し、createProfilePageコンポーネントに'use client'を追加。

--- src/app/(main)/profile/create/page.tsx
+++ src/app/(main)/profile/create/page.tsx
@@ -1,5 +1,7 @@
 import { CreateProfilePage } from '@/components/pages/createProfilePage';
 
+export const dynamic = 'force-dynamic';
+
 export default function Page() {
   return <CreateProfilePage />;
 }

2c0a3254ab9f2c56eeebe0ae3b6515a8984e4bcf
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-05-13 00:05:07 +0900

    update: InstallSectionコンポーネントにPWA判定フックを追加し、PWAでない場合のみインストールボタンを表示するように修正

--- src/components/features/signInSignUp/InstallSection.tsx
+++ src/components/features/signInSignUp/InstallSection.tsx
@@ -4,14 +4,15 @@ import { SignInSignUpButton } from '@/components/features/auth/SignInSignUpButto
 import { InstallButton } from '@/components/features/signInSignUp/InstallButton';
 import { InstallModal } from '@/components/features/signInSignUp/InstallModal';
 import { useInstallPrompt } from '@/components/features/signInSignUp/hooks/useInstallPrompt';
+import { useIsPWA } from '@/lib/hooks/useIsPWA';
 
 export const InstallSection = () => {
   const { showInstallModal, setShowInstallModal, handleInstall } =
     useInstallPrompt();
-
+  const isPWA = useIsPWA();
   return (
     <div className="flex flex-col items-center gap-4">
-      <InstallButton onClick={handleInstall} />
+      {!isPWA && <InstallButton onClick={handleInstall} />}
       <SignInSignUpButton />
       <InstallModal
         open={showInstallModal}

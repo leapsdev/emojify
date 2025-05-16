9c727acd6c0db326e8d15f0e37a2462c0ec3f425
5S <7006470+5S@users.noreply.github.com>
2025-05-16 21:11:02 +0900

    ✨ EmojiChatデプロイ:Baseメインネットにデプロイ

--- contract/ignition/modules/DeployEmojiChat.ts
+++ contract/ignition/modules/DeployEmojiChat.ts
@@ -3,7 +3,7 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 export const DeployEmojiChatModule = buildModule('DeployEmojiChat', (m) => {
   const deployer = m.getAccount(0);
 
-  const implementation = m.contract('EmojiChatV5');
+  const implementation = m.contract('EmojiChat');
 
   const initializeCalldata = m.encodeFunctionCall(
     implementation,
@@ -16,7 +16,7 @@ export const DeployEmojiChatModule = buildModule('DeployEmojiChat', (m) => {
     initializeCalldata,
   ]);
 
-  const emojiChat = m.contractAt('EmojiChatV5', proxy, {
+  const emojiChat = m.contractAt('EmojiChat', proxy, {
     id: 'EmojiChatProxy',
   });
 

b81b152f86411ad997138cbd9af16e02071d8b5e
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-05-22 08:56:40 +0900

    ✨ feat: PrivyInterfaceにlogoutメソッドを追加
    型エラーを解消するため、PrivyInterfaceにlogoutメソッドを追加しました。
    これにより、ログアウト機能が型安全に実装できるようになります。

--- src/types/privy.d.ts
+++ src/types/privy.d.ts
@@ -14,6 +14,7 @@ declare module '@privy-io/react-auth' {
     authenticated: boolean;
     user: User | null;
     login: () => Promise<void>;
+    logout: () => Promise<void>;
     getAccessToken: () => Promise<string>;
   }
 

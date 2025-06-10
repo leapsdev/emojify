9a12e565f5edeae46c6c4e211c1aa67bb771ad86
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-06-11 01:39:44 +0900

    ✨ `next.config.ts`に新しい画像ドメインとして`ipfs.io`と`dweb.link`を追加し、リモートパターンを更新。

--- next.config.ts
+++ next.config.ts
@@ -31,7 +31,7 @@ const withPWA = withPWAInit({
 const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com', 'ipfs.io'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -46,6 +46,11 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'gateway.pinata.cloud',
+        pathname: '/ipfs/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dweb.link',
         pathname: '/ipfs/**',
       },
     ],

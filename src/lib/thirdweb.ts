515b51d60ad3242f4aeb9e3734b215c03a6062ac
Yuki-Maeda-valour <y.maeda@valour-tec.com>
2025-07-08 01:51:41 +0900

    ✨ EMOJI_CONTRACT_ADDRESSを本番環境用に更新し、開発環境との整合性を保ちました。

--- src/lib/thirdweb.ts
+++ src/lib/thirdweb.ts
@@ -9,7 +9,7 @@ export const CLIENT_ID = 'af87b9c2acce067efa781dc3ea43644d';
 export const EMOJI_CONTRACT_ADDRESS =
   process.env.NODE_ENV === 'production'
     ? '0x7B94d514d87426A23d7B1D3E13e98DF6c79C3Fe8'
-    : '0xB54E5BfFaDBF798b6f3f92920E64fd1f3f59377C';
+    : '0x10f12BC253e4833834CeA5a5B78c6b85c96F3e9b';
 
 // コントラクトABI
 export const EMOJI_CONTRACT_ABI = [
